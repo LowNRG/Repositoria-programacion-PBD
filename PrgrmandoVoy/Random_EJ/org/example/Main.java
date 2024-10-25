22364ce2e31a2fd58c74f05cf803d0c3a6c53112
LAPTOP-R92VNVNM\Usuario <albertoplane96@gmail.com>
2024-10-25 15:25:17 +0200

    no message

--- PrgrmandoVoy/Random_EJ/org/example/Main.java
+++ PrgrmandoVoy/Random_EJ/org/example/Main.java
@@ -4,6 +4,8 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
         Random ejecutar = new Random();
+        ejecutar.random();
+        ejecutar.random2();
         ejecutar.random3();
     }
 }
